cd15c8157c776aadcb2cf8d68fa92a52df18ee48
machernova <machernova@samokat.ru>
2022-05-29 01:26:07 +0300

    add test

--- tests/task_2_test.py
+++ tests/task_2_test.py
@@ -6,6 +6,9 @@ class ClassificationTest(unittest.TestCase):
     def test_bicycle(self):
         self.assertEqual(MultiClassificator().predict("FAHRRAEDER // SPORTFAHRRAEDER"), "BICYCLES")
 
+    def test_usb(self):
+        self.assertEqual(MultiClassificator().predict("LEEF IBRIDGE MOBILE SPEICHERERWEITERUNG FUER IPHONE, IPAD UND IPOD - MIT LIGHTNING UND USB, 128 GB"), "USB MEMORY")
+
 
 if __name__ == '__main__':
     unittest.main()
